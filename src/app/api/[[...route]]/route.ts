e99b0ab5836e6a923931c675ff0be46adfdc9647
thais <thaisdsilve@gmail.com>
2024-09-09 19:11:58 -0300

    set up hono and zod

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,26 @@
+import { Hono } from 'hono'
+import { handle } from 'hono/vercel'
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+
+export const runtime = 'edge'
+
+const app = new Hono().basePath('/api')
+
+app.use("*", clerkMiddleware())
+app.get("/", (c) => {
+  const auth = getAuth(c);
+  if (!auth?.userId) {
+    return c.json({
+      message: "You are not logged in!"
+    })
+  }
+  return c.json({
+    message: "You are logged in",
+    userId: auth.userId
+  })
+})
+
+export const GET = handle(app)
+export const POST = handle(app)
